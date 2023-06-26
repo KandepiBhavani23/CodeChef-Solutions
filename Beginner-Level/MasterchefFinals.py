63534c7a7689d366c0ea363db88069796c695435
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-06-26 22:47:38 +0530

    Update MasterchefFinals.py

--- Beginner-Level/MasterchefFinals.py
+++ Beginner-Level/MasterchefFinals.py
@@ -1,2 +1,10 @@
-// https://www.codechef.com/problems/TOP10
+# https://www.codechef.com/problems/TOP10
+
+
+for _ in range(int(input())):
+    rank = int(input())
+    if(rank <= 10): 
+        print("YES")
+    else:
+        print("NO")
 
