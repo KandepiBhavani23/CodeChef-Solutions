4fbc2e675663d2428401f5d07b12094d7dea9271
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-06-28 08:17:00 +0530

    16_Miami GP.py

--- Beginner-Level/16_Miami GP.py
+++ Beginner-Level/16_Miami GP.py
@@ -1,1 +1,8 @@
 # https://www.codechef.com/problems/F1RULE
+
+for _ in range(int(input())):
+    fastest_finish_time, chef_time = map(int, input().split())
+    if((fastest_finish_time * 1.07) >= chef_time):
+        print("YES")
+    else:
+        print("NO")
