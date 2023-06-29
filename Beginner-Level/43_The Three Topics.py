b87fc705e8cf599be2a5b5c50944d49d156dffc1
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-06-29 11:11:40 +0530

    Create 43_The Three Topics.py

---
+++ Beginner-Level/43_The Three Topics.py
@@ -0,0 +1,12 @@
+# https://www.codechef.com/problems/THREETOPICS
+
+""" 
+total topics  : 10
+chef prepared topics = 3
+"""
+
+a, b, c, d = map(int, input().split())
+if(d <= 3):
+    print("Yes")
+else:
+    print("No")
