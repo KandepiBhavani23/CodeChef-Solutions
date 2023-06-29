9cea462d9cd10bd993f63b4f4e9828b32b511c73
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-06-29 08:05:46 +0530

    Create 34_Chefland Games.py

---
+++ Beginner-Level/34_Chefland Games.py
@@ -0,0 +1,9 @@
+# https://www.codechef.com/problems/CHEFGAMES
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    if(a == 0 and b == 0 and c == 0 and d == 0):
+        print("IN")
+    else:
+        print("OUT")
+    
